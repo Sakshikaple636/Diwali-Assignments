73f7e888bdee91d30cae68fcde28ad2b9546c02f
Sakshikaple636 <sakshikaple2003@gmail.com>
2025-10-19 21:23:59 +0530

    Add files via upload

---
+++ GenPrime.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class GenPrime  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Prime numbers up to " + n + ": ");
+        for (int num = 2; num <= n; num++) {
+            boolean isPrime = true;
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                System.out.print(num + " ");
+        }
+    }
+}
